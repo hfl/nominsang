c99e9ca43e048e8be7e30240f88e73fa1d6dcb9c
Huang Feilong <hfl@huangfeilong.com>
2023-03-16 17:12:35 +0800

    add js

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,6 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@fortawesome/fontawesome-free", to: "https://ga.jspm.io/npm:@fortawesome/fontawesome-free@6.3.0/js/all.js"
+pin "jquery", to: "admin/jquery.min.js"
+pin "bootstrap", to: "admin/bootstrap.bundle.min.js"
+pin "adminlte", to: "admin/adminlte.min.js"

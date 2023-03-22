88122863d4dac0996a898527c00c30a4cfea8c47
Huang Feilong <hfl@huangfeilong.com>
2023-03-22 17:16:16 +0800

    fix expert

--- app/controllers/admin/experts_controller.rb
+++ app/controllers/admin/experts_controller.rb
@@ -10,4 +10,43 @@ class Admin::ExpertsController < ApplicationController
     @page_title = "新增专家学者"
     @expert = Expert.new
   end
+  
+  def create
+    @expert = Expert.new(expert_params)
+    respond_to do |format|
+      if @expert.save
+        format.html {redirect_to [:admin, @expert], notice: "专家信息已保存！"}
+      else
+        format.html {render :new, status: :unprocessable_entity}
+      end
+    end
+  end
+  
+  def edit
+    @expert = Expert.find(params[:id])
+  end
+  
+  def update
+    @expert = Expert.find(params[:id])
+    respond_to do |format|
+      if @expert.update(expert_params)
+        format.html {redirect_to admin_expert_url(@expert), notice: "专家信息已更新！"}
+      else
+        format.html {render :edit, status: :unprocessable_entiy}
+      end
+    end
+  end
+  
+  def destroy
+    @expert =  Expert.find(params[:id])
+    @expert.destroy
+    respond_to do |format|
+      format.html {redirect_to admin_experts_url, status: :see_other, notice: "该专家信息已经删除！"}
+    end
+  end
+  
+  private
+  def expert_params
+    params.require(:expert).permit(:name, :alias_name, :sex, :nation, :birthday, :deathday, :occupation, :biographical__text, :avatar)
+  end
 end
--- app/models/expert.rb
+++ app/models/expert.rb
@@ -1,3 +1,4 @@
 class Expert < ApplicationRecord
   validates :name, :sex, :nation, :occupation, presence: true
+  has_one_attached :avatar
 end

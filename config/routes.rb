af89445ec29ca70c636aef1ee26290e8824917f6
Huang Feilong <hfl@huangfeilong.com>
2023-03-28 17:22:09 +0800

    fix user

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+    }
+
   namespace :admin do
     get 'dashboard/home'
     root "dashboard#home"
     resources :experts, :users, :dissertations, :noms, :books, :journals
     resource :site
   end
+
   get 'home/index'
   root "home#index"
 end
